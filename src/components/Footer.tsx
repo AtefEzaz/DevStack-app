513656ef11a4102da755270ed3adbbe226b53e8a
AtefEzaz <atefezaz22@gmail.com>
2026-09-22 17:22:41 +0600

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -58,6 +58,9 @@ export default function Footer() {
             Privacy
           </a>{" "}
           ·{" "}
+          <a href="#" className="hover:text-gray-600">
+            Terms
+          </a>
         </p>
       </div>
     </footer>
